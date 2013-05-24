354d3fa3da4256b0b5fdbde536969ea67c4b280f
noahlu <noahseo@gmail.com>
2013-05-24 14:51:55 +0800

    version 0.0.5 released

--- tests/catbus-config.js
+++ tests/catbus-config.js
@@ -4,16 +4,25 @@ var config = {
       author: "远尘 <codedancerhua@gmail.com>",
       description: "表单内的表格class必须包含table form-table well",
       level: "error",
-      // selector: "form table",
-      tagName: "table",
+      tagName: "form",
       validator: function(nodes) {
-        // console.log('in rules, result nodes length: ' + nodes.length)
-        // return true;
-        return false;
+        // console.log(nodes[0].childNodes)
+        var classes;
+        for (var i = 0; i < nodes.length; i++) {
+          // console.log(nodes[i])
+          if (nodes[i].childNodes[0].tagName == 'table') {
+            classes = nodes[i].childNodes[0].getAttribute('class');
+            // console.log('classes are: ' + classes)
+            if (!classes ||
+                classes.indexOf('table') == -1 || 
+                    classes.indexOf('form-table') == -1 ||
+                      classes.indexOf('well') == -1) {
+              return false;
+            }
+          }
+        }
+        return true;
       }
-    },
-    {
-      
     }
   ]
 };
