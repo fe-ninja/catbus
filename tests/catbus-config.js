68ce90ac36e856317d8b5ffb9e6c265c8d3249a3
noahlu <noahseo@gmail.com>
2013-07-30 20:06:03 +0800

    readd catbus-config test case

---
+++ tests/catbus-config.js
@@ -0,0 +1,37 @@
+var config = {
+  options: [
+    'html-tag-close', 
+    'html-id-duplicate', 
+    'html-meta-charset', 
+    'html-unsafe-resource', 
+    'html-https-warning', 
+    'html-hard-code', 
+    'html-doctype'
+  ],
+  rules: [
+    {
+      author: "远尘 <codedancerhua@gmail.com>",
+      id: "form-table-className",
+      description: "表单内的表格class必须包含table form-table well",
+      level: "error",
+      tagName: "form",
+      validator: function(reporter, nodes) {
+        var classes;
+        for (var i = 0; i < nodes.length; i++) {
+          if (nodes[i].childNodes[0].tagName == 'table') {
+            classes = nodes[i].childNodes[0].getAttribute('class');
+            if (!classes ||
+                classes.indexOf('table') == -1 || 
+                    classes.indexOf('form-table') == -1 ||
+                      classes.indexOf('well') == -1) {
+              reporter[this.level](this.description, nodes[i].line, null, this)
+            }
+          }
+        }
+      }
+
+    }
+  ]
+};
+
+exports.config = config;
