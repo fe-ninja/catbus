674c382c531c2670c522937540b3064dd9bab867
noahlu <noahseo@gmail.com>
2013-09-12 11:38:03 +0800

    refine rule js-object-comma regexp ignore

--- tests/jshint.js
+++ tests/jshint.js
@@ -27,6 +27,14 @@ Class({"test":'val',}) // obj extra comma
 Class({,}) 
 Class({test:1,bb:2,cc:3,})
 
+// comments 
+this.amount = new Amount({
+    element: '#' + elId + ' .j-amount-element',
+    amountText: data['amountText'],
+    amount: this._getAmountForAjax(),
+}).render()
+// comments 
+
 // should not be detected
 var reg = /{ab:cd,}/
 obj.value = obj.value.replace(/\.{2,}/g,".");
